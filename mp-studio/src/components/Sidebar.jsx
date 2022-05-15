31854bc8c19e9461c4458d19618d83cff3792360
mjdmminu99 <55178652+mjdmminu99@users.noreply.github.com>
2022-05-15 13:53:40 +0530

    change the home ui a little

--- mp-studio/src/components/Sidebar.jsx
+++ mp-studio/src/components/Sidebar.jsx
@@ -1,6 +1,13 @@
 import React from "react";
-import "./Sidebar.css";
 import SidebarItem from "./SidebarItem";
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import LightbulbRoundedIcon from '@mui/icons-material/LightbulbRounded';
+import EventNoteRoundedIcon from '@mui/icons-material/EventNoteRounded';
+import PersonSearchRoundedIcon from '@mui/icons-material/PersonSearchRounded';
+import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
+
+import "./Sidebar.css";
+
 
 
 function SideBar() {
@@ -11,11 +18,11 @@ function SideBar() {
     className="logo"
     src="/images/er.jpg" alt=""/>
 
-      <SidebarItem selected title="Home" />
-      <SidebarItem title="Learn" />
-      <SidebarItem title="Schedule" />
-      <SidebarItem title="Choreographers " />
-      <SidebarItem title="About Us " />
+      <SidebarItem Icon={HomeRoundedIcon} selected title="Home" />
+      <SidebarItem Icon={LightbulbRoundedIcon} title="Learn" />
+      <SidebarItem Icon={EventNoteRoundedIcon} title="Schedule" />
+      <SidebarItem Icon={PersonSearchRoundedIcon} title="Choreographers " />
+      <SidebarItem Icon={InfoRoundedIcon} title="About Us " />
     </div>
   );
 }
--- mp-studio/src/components/SidebarItem.jsx
+++ mp-studio/src/components/SidebarItem.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./SidebarItem.css";
 
-function SidebarItem({ selected, title }) {
+function SidebarItem({  selected, Icon, title }) {
   return (
     <div className={`sidebar-item ${selected && "selected"}`}>
+    <Icon className="sidebarItem_icon"/>
       <p className="item-title">{title}</p>
     </div>
   );
