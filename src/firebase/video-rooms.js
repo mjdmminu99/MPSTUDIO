dc6156205338ff5d995265f32cb41a15e6d6a361
mjdmminu99 <55178652+mjdmminu99@users.noreply.github.com>
2022-05-22 16:48:38 +0530

    student page also works now

--- src/firebase/video-rooms.js
+++ src/firebase/video-rooms.js
@@ -102,18 +102,7 @@ export async function createRoom() {
 }
 
 
-function joinRoom() {
-  document.querySelector('#confirmJoinBtn').addEventListener('click', async () => {
-    roomId = document.querySelector('#room-id').value;
-    console.log('Join room: ', roomId);
-    document.querySelector(
-      '#currentRoom').innerText = `Current room is ${roomId} - You are the callee!`;
-    await joinRoomById(roomId);
-  }, { once: true });
-  roomDialog.open();
-}
-
-async function joinRoomById(roomId) {
+export async function joinRoomById(roomId) {
   const db = firebase.firestore();
   const roomRef = db.collection('rooms').doc(`${roomId}`);
   const roomSnapshot = await roomRef.get();
@@ -123,14 +112,22 @@ async function joinRoomById(roomId) {
     console.log('Create PeerConnection with configuration: ', configuration);
     peerConnection = new RTCPeerConnection(configuration);
     registerPeerConnectionListeners();
-    localStream.getTracks().forEach(track => {
-      peerConnection.addTrack(track, localStream);
-    });
 
     // Code for collecting ICE candidates below
-
+    const calleeCandidatesCollection = roomRef.collection('calleeCandidates');
+    peerConnection.addEventListener('icecandidate', event => {
+      if (!event.candidate) {
+        console.log('Got final candidate!');
+        return;
+      }
+      console.log('Got candidate: ', event.candidate);
+      calleeCandidatesCollection.add(event.candidate.toJSON());
+    });
     // Code for collecting ICE candidates above
 
+    //Add remote (coach's) media streams
+    let remoteStream = new MediaStream();
+    document.querySelector('#remoteVideo').srcObject = remoteStream;
     peerConnection.addEventListener('track', event => {
       console.log('Got remote track:', event.streams[0]);
       event.streams[0].getTracks().forEach(track => {
@@ -140,11 +137,32 @@ async function joinRoomById(roomId) {
     });
 
     // Code for creating SDP answer below
+    const offer = roomSnapshot.data().offer;
+    console.log('Got offer:', offer);
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+    const answer = await peerConnection.createAnswer();
+    console.log('Created answer:', answer);
+    await peerConnection.setLocalDescription(answer);
 
+    const roomWithAnswer = {
+      answer: {
+        type: answer.type,
+        sdp: answer.sdp,
+      },
+    };
+    await roomRef.update(roomWithAnswer);
     // Code for creating SDP answer above
 
     // Listening for remote ICE candidates below
-
+    roomRef.collection('callerCandidates').onSnapshot(snapshot => {
+      snapshot.docChanges().forEach(async change => {
+        if (change.type === 'added') {
+          let data = change.doc.data();
+          console.log(`Got new remote ICE candidate: ${JSON.stringify(data)}`);
+          await peerConnection.addIceCandidate(new RTCIceCandidate(data));
+        }
+      });
+    });
     // Listening for remote ICE candidates above
   }
 }
--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -3,9 +3,11 @@ import { Button, Container } from '@material-ui/core';
 
 import React, { useEffect } from "react";
 import SideBar from "../components/Sidebar";
-
+import { joinRoomById } from "../firebase/video-rooms";
+import { useLocation } from 'react-router-dom'
 function LearnPage() {
-  useEffect(()=>{})
+  const location = useLocation();
+  useEffect(()=>joinRoomById(location.pathname.split('/').pop()))
 
   return (
     <div className='app'>
@@ -13,7 +15,7 @@ function LearnPage() {
       <Container>
         <h1>MP-STUDIO Student Portal</h1>
         <div id="videos">
-          <video id="localVideo" muted autoPlay playsInline></video>
+          <video id="remoteVideo" muted autoPlay playsInline></video>
         </div>
       </Container>
     </div>
